a867e44cc6273090712a710167ea2e7dc95fefdd
Tanvi <tanvinamjoshi3@gmail.com>
2025-05-08 17:21:47 -0400

    update slurm output & directory

--- run_finetuning_llama.py
+++ run_finetuning_llama.py
@@ -24,7 +24,7 @@ def parse_args():
     args = parser.parse_args()
 
     args.dataset = f"datasets/ft/{args.ft_dataset_name}.jsonl"
-    args.output_dir = args.output_dir if args.output_dir else f"finetuned_models/{args.ft_dataset_name}/{args.model_name}_{args.seed}"
+    args.output_dir = args.output_dir if args.output_dir else f"finetuned_models/{args.ft_dataset_name}/{args.model_name}_{args.seed}_r16"
     # args.eval_dataset = f"datasets/eval/{args.eval_dataset_name}.jsonl"
     # args.eval_output_file = f"results/{args.ft_dataset_name}/{args.model_name.split('/')[1]}_{args.eval_dataset_name}_{args.seed}.json"
     # args.base_output_file = f"results/baseline/{args.model_name}_{args.eval_dataset_name}_{args.seed}.json"
